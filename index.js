64215db380adecb58c1c02d0426addfb5c96ba79
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-04-20 10:42:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/** Force name property.
+ * Equiv to `{[someName]: someValue}[someName]`
+ * ---
+ * @param name Name of the object.
+ * @param value Object to be forced.
+ * @returns Object with name property.
+ * ---
+ * @example ```js
+ * const obj = { name: 'obj' };
+ * const obj2 = forceName('obj2', obj);
+ * console.log(obj2.name); // obj2
+ * ```
+ */
 function forceNameBuiltin(name, value) {
   return Object.defineProperty(Object(value), 'name', { 
     configurable: true,
