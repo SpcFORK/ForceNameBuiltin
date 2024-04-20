f2f4b04adf2d1d58e6eecd2da64bdc986627cb6c
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-04-20 10:25:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/** This makes the JS Runtime attach a name property to the value */
 function forceNameBuiltin(name, value) {
   return Object.defineProperty(Object(value), 'name', { 
     configurable: true,
