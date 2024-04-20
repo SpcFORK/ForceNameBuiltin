a0a15389a4862127bd13a6cf28f5bbdd40aef30e
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-04-20 10:56:00 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /** Force name property.
- * Equiv to `{[someName]: someValue}[someName]`
+ * Equiv to `{[someName]: someValue}[someName]` if the value is constructed within the object.
  * ---
  * @param name Name of the object.
  * @param value Object to be forced.
